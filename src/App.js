c0204ecb8857bc89a58f98c68565dbf0a150e315
Makar <turumberum@gmail.com>
2022-10-15 22:14:57 +0300

    ttt

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 // import { useTelegram } from './hooks/useTelegram';
-import Header from './components/Header/Header';
+import Header from "./components/Header/Header";
 // import Button from './components/Button/Button';
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import ProductList from './components/ProductList/ProductList';
-import Form from './components/Form/Form';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
+    // const {onToggleButton} = useTelegram
+    const tg = window.Telegram.WebApp;
 
-  // const {onToggleButton} = useTelegram
-  const tg = window.Telegram.WebApp
-  
-  useEffect( () => {
-    tg.ready()
-    // eslint-disable-next-line
-  }, [])
+    useEffect(() => {
+        tg.ready();
+        // eslint-disable-next-line
+    }, []);
 
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />} />
+                <Route path={"form"} element={<Form />} />
+            </Routes>
 
-  return (
-    <div className="App">
-      <Header/>
-      <Routes>
-        <Route index element={<ProductList/>}/>
-        <Route path={'form'} element={<Form/>}/>
-      </Routes>
-
-      {/* <Button onClick={onToggleButton}>Toggle</Button> */}
-    </div>
-  );
+            {/* <Button onClick={onToggleButton}>Toggle</Button> */}
+        </div>
+    );
 }
 
 export default App;
