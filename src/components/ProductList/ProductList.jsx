4e27dc044c1c1673748ebfd715ec4d99e9a04499
turumberum <72048072+turumberum@users.noreply.github.com>
2022-10-16 12:48:07 +0300

    local

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
 			queryId: queryId
 		}
 
-		fetch('http://176.10.97.60:8000/web-data', {
+		fetch('http://localhost:8000/web-data', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
